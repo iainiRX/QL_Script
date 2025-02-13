d1fb4711b7f951d87fef471a08d35addcae14449
iainiRX <3426767388@qq.com>
2025-02-13 15:45:53 +0800

    .\杜蕾斯1.0.py

---
+++ 杜蕾斯1.0.py
@@ -0,0 +1,108 @@
+#   --------------------------------注释区--------------------------------
+#   入口:杜蕾斯
+#   变量:dlsck
+#   多号分割方式 [ & 或 新建同名变量 ]
+#   完成小程序任务 抽奖, 签到, 查询
+#   corn: 一天一次即可
+#   --------------------------------祈求区--------------------------------
+#                     _ooOoo_
+#                    o8888888o
+#                    88" . "88
+#                    (| -_- |)
+#                     O\ = /O
+#                 ____/`---'\____
+#               .   ' \\| |// `.
+#                / \\||| : |||// \
+#              / _||||| -:- |||||- \
+#                | | \\\ - /// | |
+#              | \_| ''\---/'' | |
+#               \ .-\__ `-` ___/-. /
+#            ___`. .' /--.--\ `. . __
+#         ."" '< `.___\_<|>_/___.' >'"".
+#        | | : `- \`.;`\ _ /`;.`/ - ` : | |
+#          \ \ `-. \_ __\ /__ _/ .-` / /
+#  ======`-.____`-.___\_____/___.-`____.-'======
+#                     `=---='
+# 
+#  .............................................
+#           佛祖保佑             永无BUG
+#           佛祖镇楼             BUG辟邪
+#   --------------------------------代码区--------------------------------
+
+s = '''
+                                                    Powered By 神奇海螺
+                                                    QQ :3426767388
+                                                    Create at [2025-2-13 1:20]
+                                                    
+
+
+                                          ░░░░░░░░░░░░░░░░░░░░                                                              
+                                        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                        
+                                      ▒▒▒▒▒▒▒▒          ▒▒▒▒▒▒▒▒▒▒            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                      
+                                    ▒▒▒▒▒▒▒▒              ▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒                      
+                                  ░░▒▒▒▒░░░░              ░░▒▒▒▒▒▒▒▒░░  ░░░░▒▒▒▒▒▒▒▒░░░░    ░░░░░░░░▒▒░░                    
+                                ░░▒▒▒▒▒▒                    ▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒░░                ▒▒▒▒                    
+                              ▒▒▒▒▒▒▒▒▒▒                      ▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒                    ▒▒▒▒                  
+                              ▒▒▒▒▒▒▒▒▒▒          ▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒            ▒▒▒▒▒▒                
+                            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                
+                            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                
+                            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                
+                          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒              
+                          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒              
+                        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒            
+                      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░          
+                      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒          
+                      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒              ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        
+                    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                ▒▒▒▒▒▒▒▒▒▒▒▒                    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    
+                    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                    ▒▒▒▒▒▒                      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  
+                ▒▒▒▒▒▒▒▒██████████                                                    ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+            ▒▒▒▒▒▒▒▒▒▒▒▒██████████                                                        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+          ▒▒▒▒▒▒▒▒▒▒▒▒████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+        ▒▒▒▒▒▒▒▒▒▒▒▒████████████████████████████████████████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  
+          ▒▒▒▒▒▒▒▒████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██▓▓██▒▒▒▒▒▒▒▒▒▒▒▒    
+            ▒▒▒▒▒▒████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓████▒▒▒▒▒▒▒▒▒▒▒▒▒▒    
+            ▒▒▒▒████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒      
+              ▒▒██████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      
+              ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        
+                ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒          
+                  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒            
+                  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░  ▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒              
+                  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▓▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒              
+                  ▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                
+                  ▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                  
+                ▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░    ▒▒▒▒▒▒▒▒██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                    
+              ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                    
+            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                      
+            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                      
+            ▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                        
+          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                        
+          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                          
+        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░                          
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░      ▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                            
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                              
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                              
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                                
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                                  
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                                    
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░                                    
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒          ▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒                                        
+  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░                                        
+  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒            ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒                                            
+  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒            ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒                                              
+  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒          ░░▒▒▒▒▓▓██▒▒▒▒▒▒▒▒░░                                                
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒          ▒▒▒▒▒▒██▒▒▒▒▒▒▒▒                                                    
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▓▓░░░░░░░░                                                      
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                                                                
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓                                                                  
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓                                                                    
+      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓                                                                      
+      ░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓░░                                                                      
+            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓                                                                          
+                ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                                                                            
+                        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████▒▒                                                                              
+
+'''
+
+import bz2, base64
+
+exec(bz2.decompress(base64.b64decode('QlpoOTFBWSZTWZpFAbgAA9T/+f//dVVf////f///7v////5z/v///3f//5fgwABgCp0QI6fXortWK7DQZNspsWeOoIKOBqZCaCNDIEaGApPyT0TwlNgk/VPTKaempk/VNqZMR6npqD1GgaDI9RoMnqMTIAAaNBoPKBo02poJRJoaJkxBNPRMU9AGlPSemmiaPSaDT9TSNBkaNPKAAAAADQAyAAAAAAGgCKaZT0UaPUyPSD1AABkAAGgAAAAAAAAAAAAAAAAAAAAk0pQgo9RtRoaAAA0AAAAAAAAAAAAAAAAAABoADQAEGExAZMmgYmgZNDJoaDTCNGgGIZMgwmQaYjQZDQYgaGRkABk00yYmIaAAkRINTQiJ6j1BnqhkAyAAaNDQAAAAAAAADQAyAAAaGgDTQAxDVTnljCHdlpwIyE31hCbokFhIat3Dcj3MAk51NelTXjkGCqrEiWu2rCtWysvdPiOiQSEGNXkyyXjlJIe1ElHuBJA5RBGab4fBESIpgyoi04VmZVMmagb5HQVJkwXN7YVcK1ZVCaS7pYaAbkCEX0hrTsTdnKu3TVmXMa+3+bxlTSYngv+W67cp9P6OEyXttj2/T1VTR1LTBAhwyixhIQ5BgSSZEmrEkIQZtGYPRwoUFiCkiCDLSyQzJZIiFTFswjKUpJUkqiC/nkwGYymMuRXBYWoDi4VMUsGYxLMDYwqjExaLERVto2lLazy8K+nK3JmNBDZWZ8YAqDooSpDv7QNBc7RBIDQDM00AiQnGIOki1xzH2MRQyOQlxrhZxvMOpfGjPeXHDTTST8qdOfZTbCEw8FKSERwjwuSKGsb3rpzrrVFxJ6y2hMaEJaumpIqdxKn4LsuMG73jmMaLenFpHC3H6cAaqdJ+lSwaF+qmlbGqayppHnOpxSGzCTaqMVOnU0GgVqA0ARRPGoEUZrMYQKcZxQUkIMAF0sJIXNQChiSlKOMADJRbYhqRvrvQJjFrnWxfZK/rn9GYYpbHV0RJWM/iM6JwOTLdxUUbnEFYuVwX42EgOPcwteu9zb9igXEeJ0kEM1e5TKpykhKamzK3BrkI41gWHU3aeTos0BwRi1QSosjDvKRZKoGoU2OMqEgoWjhcB/Stw7SdLHakKKfPLmDBQ1MrBjLrGP3xBb52OqzOxajhw+5gmYpE2LeNI9nBXi39WZ2IwJJLAaTaJKqGVhrDAw+fUdW7OBzEQxZaNAmBAVMByCctDWWlDuRXoAB7fbw8DEt83AbCAGOR9JDqM+xithw3EkC33nwBxVOdKHLU80TpX80N5Iga81sw4D8x0748hagpQ/EkwXsyz6OjudDwR9J7azHQeklp8uBhLrB5LmQm2GN3CviPbtRiWirbZSzrpiBS9yFJsId8n1fUk17sznNzXsMo1usmdk+s9gbuOiqyWjtY7w24eXE8cIJWsVgUhUwwSELNEBpRHCMGNAE6kBARYvDB9q6VQChOpwTTZlkk07L70WEY9jOZxYIsVnPn1HEEzeNI/GTMjhaNH343D8AwTonnA9AkCDCRBfJngiBUxDDjkCGkPBBACmgmawENgPuQ6MiKiQhaJISpJDpFZmS+9E2xrvYoo4G8ohfzcxgq2s+WUEJKoYZLoysdDda91KkEOTPiv723YCQARAIjXA3prCyDDbkIlpGSQjCYiIkbYEPZ1xrmOpzVtlxgWHgFMTMcFQUVOJyNFQzAodBGgCktyWI5+IYMtsOdlszhFHuaYhou+KM+vm9Dox1hiyj7kheJ4RryPR2+ONnNbjui3xdxmWqg2gyiajpymfYn3T19V2sAc24XKhDE1SuiDuQnBEg5+leQSUcPh2OXq5bUfJbSAxSjSJSzNM2IsFqKRay5kvtKaqQKaemmoqNKhHEFmT2JnuLoFHBuTd4bwNEpxEcVCqsy8Px2JLAyaCTZikM/ejjb11GRUIa2vs6xm8SrhzqVoFSiXTv5VVOE+NKwsLGFkrtJZJGzGXlxt6biDd1qJwkSzrlVRR+yXqmakqoM2MlI1hgiRIrw6SE7Mh81mS426uU3HMFyYYY5ohJVQiLVc2M8skQ7BIZ2xGo0Kq5xImLpUcJYQxNBT4LPiMAqoDisa1yYF6ochrgXidZFRAy7cKI5UnH61iUTNC+Y6DwQQVtF06GKDQGqRG8pHkfjQXYgrmjhnBb4WQlFRExzpDPhLduHEalL9GGMjNN/IQnge0HXDBjJKulGm0GMcYZ9cktYdoy/QUXaWPv6uFLFwRNq9YLZpUzbqwAUG4dUSdZa6JDQhr2WvBBSmDTGFF1AxG0vVhBtVNBSOS0/oyxi189yID0Nm0D0KNWhKRC4pk4vPgvKXYqy02FrFoSaBjKStRjUlxEM0nHUllIbICg8jnSRq8WLnbTgJ3gwKTBfGpKUAIhgxGwtRVeDW41Ops78ojMy9YVqXClOACo+2ZLQwYKxdJtrfTj0d3hqaPAmgRKnBozaVoYqMJS5k5RVxEBn1jGoHZkGVSs6lsGIoJKTCUzZeCiUgQNEDQ22ufbWnINsjMq819FhdK7KKVFbdFAbujJI82bA5PMHJYVE0VHXAgGMXGwTW7ziJrbLHRwigxHUI+QtaBKo4LdLFyRVeY8rFsycG/IST4LzVZ1SMbZSeuqMMGbVhYjU2s120VG1VggYMawghIZN0xdF1rpaTQRQomQaO6k495ybtOizOElKrlyH3CrTIlVCkUJiw5I6OVVMFk9NEq6sQE9ZFRJr1ybmVHEsj94fBs/BKDImndvbugQGqTyBPOVl5oUDszp/CEaROyYR+hcfq+AEon0zSVgkPv5aVl6RhCUtk0L6T4Wbs6j7HcbQ+3/Nl6SsDZ+h4gWrbDCBCSCaSqXwQGyfp+pRbPa+vM/VN6oka/Uws/OD+WCEeMxAlRgCSElOlNuPP3BQ0Ks/TbgJJCAtRKZc8FU/Erfry1IcMdEfGY7D8XsqAibUXZSWxb+WOIYXdrrTQRPEgiIK+RQGOVX5eRT+WjnLWkc8qSzXqxAY4mW2+kL7w8wy1Vf0pce216olQ/r/UMyTkfIk12Bis/kyv+fkIGIqZAUe6WF2w/jCaYoiLGQiyLIKAMQiwAWEdZbFBFtpCJRttQVYLCMbYVBZaSkkmsgFikTFKCIFQnyebQ4r0qGt+rk8V6uPdaCUhCDGwVjQjmGDaGwKtBY0BAPCTlkgHepMowiwzI2hWFQmZiECq0JFjSliuaSJYFjscjSJtgcQIQl2bEZWklg1iYXtXN50JSyWNqYvtok8Ie0hISRkaX7tKrErGFXRoWVoK44SseFCAowQBgyjEtq2wDOYIMrSVjJsgxsCtYSElaxXuGK+2BJ3wIFRjYFzStYEIsMOTSughqWZXRBkz0cUFgGRKyCh+KQuLAkMg1muoZGKQWZrZpJbZlSYEAhWBWEz0JhNK2ZGQVQAEYomawC4SihCVgahMRhPwdJCExGQFg5bIBVYZkBSSGuJM9kAyMFDSF2zMbEg0uOEkhXMRUaAh5WxkwyAskAwwkCa2JA/QZDTQzl0qKCkzkA0Ega/r1khMiLCAsJIGijpmdgITOSQMaiwgwIyZmCIottsD0H9rYFidV3mB1hiuDZ6+8AjGNxc693F/k8pj1zd7Q2rn98DKjODsbrRATm99P7ODNDzICURDizhv1cDQO+iTvY6b0lLBR4iuKNjEWzDZZc1s6hps7wNbjPYmR3ofwB3YwrLj/t8PH/5DiWdl2bof+LuSKcKEhNIoDcA')))
